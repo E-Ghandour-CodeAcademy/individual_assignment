5408df92414db12acb343552c933aaec4e60e426
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-11-20 21:42:47 +0200

    Modified the code with type annotation and changed the html templates for results and subscribe to allow only logged in users to subscribe

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import requests
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, Response
 from flask_pymongo import PyMongo
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from typing import Optional
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/BookStore"
 app.config['SECRET_KEY'] = 'very_strong_secret_key'
 mongo = PyMongo(app)
-login_manager = LoginManager() 
+login_manager = LoginManager()
 login_manager.init_app(app)
 
 class User(UserMixin):
-    def __init__(self, user_id):
+    def __init__(self, user_id: str):
         self.id = user_id
 
 @login_manager.user_loader
-def load_user(user_id):
+def load_user(user_id: str) -> Optional[User]:
     user = mongo.db.users.find_one({"_id": user_id})
     if user:
-        return User(user_id) 
+        return User(user_id)
     return None
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register() -> Response:
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username: str = request.form['username']
+        password: str = request.form['password']
         user = mongo.db.users.find_one({'_id': username})
         if user:
             return render_template('register.html', message="User already exists. Please log in.")
@@ -35,41 +36,41 @@ def register():
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login() -> Response:
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username: str = request.form['username']
+        password: str = request.form['password']
         user = mongo.db.users.find_one({'_id': username})
         if user:
             user = mongo.db.users.find_one({'_id': username, 'password': password})
             login_user(User(username))
             return redirect(url_for('dashboard'))
         else:
-            return render_template('login.html', message="User does not exists. Please register.")
+            return render_template('login.html', message="User does not exist. Please register.")
     return render_template('login.html')
 
 @app.route('/logout')
 @login_required
-def logout():
+def logout() -> Response:
     logout_user()
     return redirect(url_for('home'))
 
 @app.route('/dashboard')
 @login_required
-def dashboard():
+def dashboard() -> Response:
     subscriptions = mongo.db.subscriptions.find({'email': current_user.id})
     return render_template('dashboard.html', subscriptions=subscriptions)
 
 @app.route('/')
-def home():
+def home() -> Response:
     return render_template("home.html")
 
 @app.route('/search')
-def search():
-    url = "https://openlibrary.org/search.json?q="
-    fields = "&fields=title,author_name,first_publish_year,key"
-    query = request.args.get('query')
-    sort_order = request.args.get('sort')
+def search() -> Response:
+    url: str = "https://openlibrary.org/search.json?q="
+    fields: str = "&fields=title,author_name,first_publish_year,key"
+    query: Optional[str] = request.args.get('query')
+    sort_order: Optional[str] = request.args.get('sort')
 
     try:
         response = requests.get(url + query.replace(" ", "+") + fields + "&sort=" + sort_order)
@@ -79,9 +80,9 @@ def search():
         return f"An error occurred: {e}"
 
     expected_keys = ['author_name', 'first_publish_year', 'title']
-    html_content = ""
+    html_content: str = ""
     for item in json_data:
-        book_info = "<div class='book'>"
+        book_info: str = "<div class='book'>"
         for key in expected_keys:
             value = item.get(key, 'Unknown')
             if isinstance(value, list):
@@ -93,20 +94,20 @@ def search():
 
     if response.json()['numFound'] == 0:
         html_content = "<p>No results found for your search!</p>"
-    
+
     return render_template("results.html", html_content=html_content, query=query)
 
 @app.route('/subscribe', methods=['GET', 'POST'])
-def subscribe():
+@login_required
+def subscribe() -> Response:
     if request.method == 'POST':
-        email = request.form['email']
-        title = request.form['title']
+        title: str = request.form['title']
+        email: str = current_user.id
         mongo.db.subscriptions.insert_one({'email': email, 'title': title})
         return render_template("thank_you.html")
     else:
-        title = request.args.get('title', 'Unknown Title')
+        title: str = request.args.get('title', 'Unknown Title')
         return render_template('subscribe.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
